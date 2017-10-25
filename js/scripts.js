646db26cbb459e3d0ea7d152c75d62e6f2fa8421
Ginger Lee Kretschmer and Nathan Barrett <nathan@>
2017-10-25 16:06:16 -0700

    refractor back-end js

--- js/scripts.js
+++ js/scripts.js
@@ -3,34 +3,32 @@ var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
 var pigWord = function(word) {
   for (var i = 0; i < vowels.length; i++) {
     if (vowels[i] === word[0]){
-      return word + "way"
-    } else if (vowels[i] !== word[0]){
-      var splitWord = word.split("");
-      var firstLetter = splitWord.shift();
-      var ewWord = splitWord.join("") + firstLetter + "ay";
-      return ewWord
+      return word + "way";
+    } else if ((vowels[i] !== word[0]) && (word[1] === "q") && (word[2] === "u")) {
+      var newWord = word.substr(3) + word.substr(0,3) + "ay"
+    } else if ((word[0] === "q") && (word[1] === "u")) {
+      var newWord = word.substr(2) + word.substr(0,2) + "ay"
+    } else if ((vowels[i] !== word[0]) && (vowels[i] !== word[1]) && (vowels[i] !== word[2])) {
+      var newWord = word.substr(3) + word.substr(0,3) + "ay";
+    } else if ((vowels[i] !== word[0]) && (vowels[i] !== word[1])) {
+      var newWord = word.substr(2) + word.substr(0,2) + "ay";
+    } else if (vowels[i] !== word[0]) {
+      var newWord = word.substr(1) + word.substr(0,1) + "ay";
     }
+    return newWord;
   };
-
+};
 $(document).ready(function() {
   $("form#pig-form").submit(function(event) {
     event.preventDefault();
     var word = $("input#piggies").val();
     var newWord = pigWord(word);
-    console.log(newWord);
-    debugger
+    $(".result").show();
+    $("#pigWord").text(newWord);
   });
 });
 
 
-
-
-
-// for (var i = 0; i = [0]; i++) {
-//   for (var y = 0; y < vowels.length; y++) {
-//     if (word[i] === vowels[y]) {
-//       newWord.push["way"]
-//       var result = newWord.join()
 
 
 // var isAlpha = function(char) {
