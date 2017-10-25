e4b357dd32caa83c860f9cfdbaf1dbfb4a0be6f5
Ginger Lee Kretschmer and Nathan Barrett <nathan@>
2017-10-25 16:57:14 -0700

    debug a little bit

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+// var pigPhrase = function(sentence) {
+//   var pigArray = sentence.replace(/[^a-z0-9 ]+ig, "").toLowerCase().split(" ");
+//   var newArray = pigArray.map(pigWord);
+//   sentence = newArray.join(" ");
+//   return sentence
+// }
+
+
 
 var pigWord = function(word) {
   for (var i = 0; i < vowels.length; i++) {
+    debugger
     if (vowels[i] === word[0]){
-      return word + "way";
+      var newWord = word + "way";
     } else if ((vowels[i] !== word[0]) && (word[1] === "q") && (word[2] === "u")) {
-      var newWord = word.substr(3) + word.substr(0,3) + "ay"
+      var newWord = word.substr(3) + word.substr(0,3) + "ay";
     } else if ((word[0] === "q") && (word[1] === "u")) {
-      var newWord = word.substr(2) + word.substr(0,2) + "ay"
-    } else if ((vowels[i] !== word[0]) && (vowels[i] !== word[1]) && (vowels[i] !== word[2])) {
+      var newWord = word.substr(2) + word.substr(0,2) + "ay";
+    } else if ((vowels[i] !== word[0]) && (vowels[i] !== word[1]) && (vowels[i] !==  word[2])) {
       var newWord = word.substr(3) + word.substr(0,3) + "ay";
     } else if ((vowels[i] !== word[0]) && (vowels[i] !== word[1])) {
       var newWord = word.substr(2) + word.substr(0,2) + "ay";
