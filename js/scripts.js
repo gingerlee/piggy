6606ce566c790d3f304fa93c85eda3c17a9632ef
Ginger Lee Kretschmer and Nathan Barrett <nathan@>
2017-10-25 13:42:44 -0700

    add else if statement to return pig latin for a word starting with a consonant

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,40 @@
-var splitWord = word.split("");
-var vowels = ["a", "A" "e", "E", "i", "I", "o", "O", "u", "U"];
-var newWord = [];
-var way = "way";
-
+var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
 
 var pigWord = function(word) {
-  for (var i = 0; i = [0]; i++) {
-    for (var y = 0; y < vowels.length; y++) {
-      if (word[i] === vowels[y]) {
-      return word + way;
-      }
+  for (var i = 0; i < vowels.length; i++) {
+    if (vowels[i] === word[0]){
+      return word + "way"
+    } else if (vowels[i] !== word[0]){
+      var splitWord = word.split("");
+      var firstLetter = splitWord.shift();
+      var ewWord = splitWord.join("") + firstLetter + "ay";
+      return ewWord
     }
-  }
-};
+  };
+
+$(document).ready(function() {
+  $("form#pig-form").submit(function(event) {
+    event.preventDefault();
+    var word = $("input#piggies").val();
+    var newWord = pigWord(word);
+    console.log(newWord);
+    debugger
+  });
+});
 
 
-var isAlpha = function(char) {
-  if ((char >= "A") && (char <= "z"){
-    return true
-  }
-}
+
+
+
+// for (var i = 0; i = [0]; i++) {
+//   for (var y = 0; y < vowels.length; y++) {
+//     if (word[i] === vowels[y]) {
+//       newWord.push["way"]
+//       var result = newWord.join()
+
+
+// var isAlpha = function(char) {
+//   if ((char >= "A") && (char <= "z"){
+//     return true
+//   }
+// }
